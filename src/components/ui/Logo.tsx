096f9eaa218026e8112ecabbbfb3ddc64b77fc66
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-21 18:25:19 +0530

    Turned logo into a link to the home page

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
-    <span className="block w-fit mx-auto px-3 py-1 border-2 border-dark-text text-dark-text text-2xl font-bold tracking-widest shadow-md text-center">
+    <Link
+      to="/"
+      className="block w-fit mx-auto px-3 py-1 border-2 border-dark-text text-dark-text text-2xl font-bold tracking-widest shadow-md text-center">
       UMS
-    </span>
+    </Link>
   );
 }
