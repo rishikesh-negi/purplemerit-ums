e2ca579852a3169365335561bc2816713c44ac45
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-22 18:13:17 +0530

    Added a constant for number of data rows in a paginated table

--- src/utils/appConstants.ts
+++ src/utils/appConstants.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = "http://localhost:8000/api/v1";
 export const SUCCESS_TOAST_ID = "app-toast-success";
 export const FAIL_TOAST_ID = "app-toast-fail";
+export const DATA_ROWS_PER_PAGE = 10;
